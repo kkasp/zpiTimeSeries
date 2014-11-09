fc891dd7b6e267b84ae749ef7f89a09f4ded770a
Mateusz Maj <mmaj269@gmail.com>
2014-11-09 20:00:49 +0100

    pierwszy commit- generowanie wykresów

---
+++ ChartPanel/src/com/project/zpi/XYChartValues.java
@@ -0,0 +1,53 @@
+package com.project.zpi;
+
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
+
+public class XYChartValues {
+	
+	String name;
+	ArrayList<Point2D> values;
+	int size;
+	
+	/*****************************************************************************************/
+	/* 									KONSTRUKTORY 										 */
+	/*****************************************************************************************/
+	
+	public XYChartValues(String name, ArrayList<Point2D> values){
+		
+		
+		this.name = name;
+		this.values = values;
+		//----------
+		
+		size = values.size();
+	}
+	
+	/*****************************************************************************************/
+	/* 									GETTERY I SETTERY 								 	 */
+	/*****************************************************************************************/
+	
+	/* SETTERY: */
+	
+	public void setName(String name){
+		this.name = name;
+	}
+	
+	public void setValues(ArrayList<Point2D> val){
+		this.values = val;
+	}
+	
+	/* GETTERY */
+	
+	public String getName(){
+		return this.name;
+	}
+	
+	public ArrayList<Point2D> getValues(){
+		return this.values;
+	}
+	
+	public int getSize(){
+		return this.size;
+	}
+}
